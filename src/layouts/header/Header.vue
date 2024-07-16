1d8f8ac4778918e5a1d78fa4a43cebb2c4cfe710
Osama Alaaeldin <osama.alaaeldin@wiot360.com>
2024-07-16 17:26:57 +0400

    InitialCommit

---
+++ src/layouts/header/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app-bar app clipped-left clipped-right color="primary" dark>
+    <v-app-bar-nav-icon
+      class="d-block d-md-none"
+      @click="$vuetify.breakpoint.smAndDown ? setSidebarDrawer(!Sidebar_drawer) : $emit('input', !value)"
+    />
+    <v-spacer />
+    <!---right part -->
+    <v-btn dark color="success" class="text-capitalize">Workshop Module</v-btn>
+    
+  </v-app-bar>
+</template>
+<script>
+// Utilities
+import { mapState, mapMutations } from "vuex";
+export default {
+  name: "Header",
+
+  components: {},
+
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data: () => ({
+    userprofile: [
+      { title: "My Profile" },
+      { title: "My Balance" },
+      { title: "Inbox" },
+      { title: "Account Setting" },
+      { title: "Logout" }
+    ],
+    href() {
+      return undefined;
+    }
+  }),
+
+  computed: {
+    ...mapState(["Sidebar_drawer"])
+  },
+
+  methods: {
+    ...mapMutations({
+      setSidebarDrawer: "SET_SIDEBAR_DRAWER"
+    })
+  }
+};
+</script>
