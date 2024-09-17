a05020b0e712389b18ae3b711aa6e65cf8930a5f
Osama Alaaeldin <osama.alaaeldin@wiot360.com>
2024-09-17 16:18:22 +0400

    [4-move-sidebar-into-main-global-folder]

---
+++ src/components/commonComponents/Persons/person-avatar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            info: { type: Object, default: () => {} },
+            showExtraInfo: {
+                Type: Boolean,
+                default: () => {false}
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
